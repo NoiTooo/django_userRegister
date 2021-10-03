753fa53830acd5af8b60ac697ae6d642c60df71c
NoiTooo <t.komenoi@gmail.com>
2021-10-03 14:56:10 +0900

    first commit

---
+++ project/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.conf import settings
+import debug_toolbar
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('register.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
